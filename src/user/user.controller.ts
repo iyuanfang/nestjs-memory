2bf55cddd2087bdb28c657159fa1a38e587e1e3f
yuanfang <65866693@qq.com>
2020-11-28 14:31:37 +0800

    修改prettier自动不带分号，习惯就好

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -3,37 +3,39 @@ import {
   Controller,
   Delete,
   Get,
+  HttpException,
+  HttpStatus,
   Param,
   Post,
   Put,
-} from '@nestjs/common';
-import { UserService } from './user.service';
+} from '@nestjs/common'
+import { UserService } from './user.service'
 
 @Controller('user')
 export class UserController {
   constructor(private userService: UserService) {}
   @Get()
   findAll(): string {
-    return this.userService.findAll();
+    return this.userService.findAll()
   }
 
   @Get(':id')
   find(@Param('id') id: string): string {
-    return `Return a #${id} user`;
+    return this.userService.find(id)
   }
 
   @Post()
   create(@Body() createCatDTO: any) {
-    return 'Create a user';
+    return 'Create a user'
   }
 
   @Put(':id')
   update(@Body() updateCatDTO: any, @Param('id') id: string) {
-    return `Update a #${id} user`;
+    return `Update a #${id} user`
   }
 
   @Delete(':id')
   delete(@Param('id') id: string): string {
-    return `Delete a #${id} user`;
+    return `Delete a #${id} user`
   }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,8 +1,15 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common'
 
 @Injectable()
 export class UserService {
   findAll(): string {
-    return 'Find all user';
+    return 'Find all user'
+  }
+  find(id: string): string {
+    if (id == '2') {
+      throw new HttpException('Forbidden', HttpStatus.FORBIDDEN)
+    } else {
+      return `Return a #${id} user`
+    }
   }
 }
