76d2e6fc874d854099002270e5d52a0337d41137
yuanfang <65866693@qq.com>
2020-11-28 12:28:41 +0800

    放入user module中

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,29 +1,39 @@
-import { Body, Controller, Delete, Get, Param, Post, Put, Req } from "@nestjs/common";
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
+import { UserService } from './user.service';
 
-@Controller("user")
+@Controller('user')
 export class UserController {
+  constructor(private userService: UserService) {}
   @Get()
   findAll(): string {
-    return "all user";
+    return this.userService.findAll();
   }
 
-  @Get(":id")
-  find(@Param("id") id: string): string {
+  @Get(':id')
+  find(@Param('id') id: string): string {
     return `Return a #${id} user`;
   }
 
   @Post()
-  create(@Body() createCatDTO:any){
-      return "Create a user"
+  create(@Body() createCatDTO: any) {
+    return 'Create a user';
   }
 
-  @Put(":id")
-  update(@Body() updateCatDTO:any,@Param("id") id:string){
-      return `Update a #${id} user`;
+  @Put(':id')
+  update(@Body() updateCatDTO: any, @Param('id') id: string) {
+    return `Update a #${id} user`;
   }
 
-  @Delete(":id")
-  delete(@Param("id") id:string):string{
-      return `Delete a #${id} user`;
+  @Delete(':id')
+  delete(@Param('id') id: string): string {
+    return `Delete a #${id} user`;
   }
 }
